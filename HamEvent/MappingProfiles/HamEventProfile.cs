13687dfeb6206dfc30f19321da1dd2a3eaea2b55
Bogdan-Ioan BRUDIU <bogdan@brudiu.ro>
2023-05-13 12:13:29 +0300

    adif parser

---
+++ HamEvent/MappingProfiles/HamEventProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using M0LTE.AdifLib;
+
+namespace HamEvent.MappingProfiles
+{
+  public class HamEventProfile : Profile
+  {
+    public HamEventProfile()
+    {
+            CreateMap<AdifContactRecord, QSO>()
+                  .ForMember(dest => dest.Callsign1, act => act.MapFrom(src => src.StationCallsign))
+                  .ForMember(dest => dest.Callsign2, act => act.MapFrom(src => src.Call))
+                  .ForMember(dest => dest.RST1, act => act.MapFrom(src => src.RstSent))
+                  .ForMember(dest => dest.RST2, act => act.MapFrom(src => src.RstReceived))
+                  .ForMember(dest => dest.Mode, act => act.MapFrom(src => src.Mode))
+                  .ForMember(dest => dest.Band, act => act.MapFrom(src => src.Band))
+                  .ForMember(dest => dest.Timestamp, act => act.MapFrom(src => src.QsoStart));
+    }
+  }
+}
--- HamEvent/Program.cs
+++ HamEvent/Program.cs
@@ -1,9 +1,11 @@
+using Microsoft.AspNetCore.Hosting;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllersWithViews();
-
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.

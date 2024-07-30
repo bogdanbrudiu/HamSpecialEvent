a6f4b9047b75f9c37b5d767e4d05d1796255803a
Bogdan-Ioan BRUDIU <bogdan@brudiu.ro>
2024-07-30 09:30:12 +0300

    angular 18

--- HamEvent/ClientApp/src/app/app.module.ts
+++ HamEvent/ClientApp/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
-import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { HttpClient, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { NgxPaginationModule } from 'ngx-pagination';
 
 import { AppComponent } from './app.component';
@@ -23,51 +23,40 @@ export function createTranslateLoader(http: HttpClient) {
   return new TranslateHttpLoader(http, './assets/i18n/', '.json');
 }
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    NavMenuComponent,
-    HomeComponent,
-    QSOsComponent,
-    EventsComponent,
-    EventTopComponent,
-    DashboardComponent,
-    AdminQSOsComponent,
-    AdminEventComponent,
-    UploadComponent,
-    SanitizedHtmlPipe
-  ],
-  imports: [
-    BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
-    TranslateModule.forRoot({
-      defaultLanguage: 'en',
-      loader: {
-        provide: TranslateLoader,
-        useFactory: (createTranslateLoader),
-        deps: [HttpClient]
-      }
-    }),
-    NgxPaginationModule,
-    HttpClientModule,
-    FormsModule,
-    ReactiveFormsModule,
-    RouterModule.forRoot([
-      
-      { path: 'Home', component: HomeComponent, pathMatch: 'full' },
-      { path: '', component: EventsComponent, pathMatch: 'full' },
-      { path: 'Events', component: EventsComponent, pathMatch: 'full' },
-      { path: ':id/top', component: EventTopComponent, pathMatch: 'full' },
-      { path: ':id/live', component: DashboardComponent, pathMatch: 'full' },
-
-      
-      { path: ':id/:secret/edit', component: AdminEventComponent, pathMatch: 'full' },
-      { path: ':id/:secret', component: AdminQSOsComponent, pathMatch: 'full' },
-
-      { path: ':id', component: QSOsComponent, pathMatch: 'full' },
-    ])
-  ],
-  providers: [],
-  exports: [SanitizedHtmlPipe],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [
+        AppComponent,
+        NavMenuComponent,
+        HomeComponent,
+        QSOsComponent,
+        EventsComponent,
+        EventTopComponent,
+        DashboardComponent,
+        AdminQSOsComponent,
+        AdminEventComponent,
+        UploadComponent,
+        SanitizedHtmlPipe
+    ],
+    exports: [SanitizedHtmlPipe],
+    bootstrap: [AppComponent], imports: [BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
+        TranslateModule.forRoot({
+            defaultLanguage: 'en',
+            loader: {
+                provide: TranslateLoader,
+                useFactory: (createTranslateLoader),
+                deps: [HttpClient]
+            }
+        }),
+        NgxPaginationModule,
+        FormsModule,
+        ReactiveFormsModule,
+        RouterModule.forRoot([
+            { path: 'Home', component: HomeComponent, pathMatch: 'full' },
+            { path: '', component: EventsComponent, pathMatch: 'full' },
+            { path: 'Events', component: EventsComponent, pathMatch: 'full' },
+            { path: ':id/top', component: EventTopComponent, pathMatch: 'full' },
+            { path: ':id/live', component: DashboardComponent, pathMatch: 'full' },
+            { path: ':id/:secret/edit', component: AdminEventComponent, pathMatch: 'full' },
+            { path: ':id/:secret', component: AdminQSOsComponent, pathMatch: 'full' },
+            { path: ':id', component: QSOsComponent, pathMatch: 'full' },
+        ])], providers: [provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule { }
